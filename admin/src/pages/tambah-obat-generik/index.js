a062d96b7f4957090cc6bb3019c640c1f9a83025
Gesang Paudra J <g.paudra@gmail.com>
2023-10-29 14:53:20 +0700

    add page tambah obat generik

---
+++ admin/src/pages/tambah-obat-generik/index.js
@@ -0,0 +1,15 @@
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+
+
+const TambahObatGenerik = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+
+      </Grid>
+    </Grid>
+  )
+}
+
+export default TambahObatGenerik
